8a333ffd4f54fd79df369443c84e5411c521d4a6
Mohamed Al Arabi <mo.arabi@gmail.com>
2025-04-16 02:32:11 +0200

    set config to static export

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ const { withContentlayer } = require('next-contentlayer2')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     deviceSizes: [
       360, 414, 512, 640, 750, 828, 1080, 1200, 1536, 1920, 2048, 3840,
     ],
